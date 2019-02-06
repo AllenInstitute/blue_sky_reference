951ba02f17c883deb997749a6fe5e22dd7fb521e
Tim Fliss <timf@alleninstitute.org>
2019-02-05 16:10:45 -0800

    CONNP-434; implement DAG as graph rather than tree.

--- restart_notebook.sh
+++ restart_notebook.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-pkill -9 -f "circus_test"
-
-source activate base
+pkill -9 -f "shell_plus"
 
 export BASE_DIR=/blue_sky
 
-rm ${BASE_DIR}/logs/circus.log
+rm ${BASE_DIR}/logs/nb.log
 
 export WORKFLOW_CONFIG_YAML=$(python -c "import ${DJANGO_SETTINGS_MODULE} as settings; print(settings.WORKFLOW_CONFIG_YAML)")
 export APP_PACKAGE=$(python -c "import ${DJANGO_SETTINGS_MODULE} as settings; print(settings.APP_PACKAGE)")
 export MESSAGE_QUEUE_HOST=$(python -c "import ${DJANGO_SETTINGS_MODULE} as settings; print(settings.MESSAGE_QUEUE_HOST)")
 export MESSAGE_QUEUE_PORT=$(python -c "import ${DJANGO_SETTINGS_MODULE} as settings; print(settings.MESSAGE_QUEUE_PORT)")
 
-DEBUG_LOG=${BASE_DIR}/logs/circus.log /bin/bash -c 'source activate circus; unset DJANGO_SETTING_MODULE; cd /blue_sky_workflow_engine/circus; /opt/conda/envs/circus/bin/circusd --daemon circus.ini; celery -A workflow_client.tasks.circus_test worker --concurrency=1 --loglevel=info -n circus@blue_sky'
+source activate base
 
+echo 'STARTING NOTEBOOK WORKER'
+#/bin/bash -c 'source activate nb; DEBUG_LOG=${BASE_DIR}/logs/nb.log nohup python -m manage shell_plus --notebook 2>&1 >> /dev/null &'
+source activate nb; DEBUG_LOG=${BASE_DIR}/logs/nb.log python -m manage shell_plus --notebook
+sleep 10
+echo 'done'
