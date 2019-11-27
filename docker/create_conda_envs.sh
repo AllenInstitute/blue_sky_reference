2869b74dbb51ebdb30a089c17be62770cb1fab76
Zach Madigan <zach.madigan@alleninstitute.org>
2019-11-27 12:09:58 -0800

    Remove semantic_requirements.txt from create_conda_envs.sh

--- docker/create_conda_envs.sh
+++ docker/create_conda_envs.sh
@@ -33,7 +33,6 @@ yes | ${PIP_INSTALL} -r /source/blue_sky_workflow_engine/test_requirements.txt
 yes | ${PIP_INSTALL} -r /source/${APP_NAME}/requirements.txt
 yes | ${PIP_INSTALL} -r /source/${APP_NAME}/test_requirements.txt
 yes | ${PIP_INSTALL} -r /source/blue_sky_workflow_engine/nb_requirements.txt
-yes | ${PIP_INSTALL} -r /source/blue_sky_workflow_engine/semantic_requirements.txt
 
 #conda create -q --yes --prefix=${NB} --clone ${PY_37}
 #source activate ${NB} && (yes | ${PIP_INSTALL} -r /blue_sky_workflow_engine/nb_requirements.txt)
